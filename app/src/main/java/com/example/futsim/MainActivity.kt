660087e8c80feb55bf6bf6682352e3b7dc9320ee
Gabriel <gl9736387@gmail.com>
2025-05-12 18:21:49 -0300

    test(screens): Adiciona telas para o testes de navegação

--- app/src/main/java/com/example/futsim/MainActivity.kt
+++ app/src/main/java/com/example/futsim/MainActivity.kt
@@ -4,27 +4,35 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import com.example.futsim.ui.theme.FutSimTheme
+import androidx.navigation.compose.rememberNavController
+import com.example.futsim.ui.telas.TelaInicial
+import com.example.futsim.ui.telas.TelaPrincipal
+import com.example.futsim.ui.telas.TelaTeste
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             FutSimTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                val navController = rememberNavController()
+                NavHost(navController = navController, startDestination = "tela_inicial"){
+                    composable("tela_inicial") {TelaInicial(navController)}
+                    composable("tela_principal") {TelaPrincipal(navController)}
+                    composable("tela_teste") {TelaTeste(navController)}
+
                 }
+
             }
         }
     }
