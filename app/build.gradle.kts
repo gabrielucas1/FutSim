05f6f10971f3975ae31f3c313054ca0afdeb7c89
Luiz-Andalezio <luizandalecio19@gmail.com>
2025-06-05 15:46:10 -0300

    build(gradle): aprimora as configurações de build e dependências

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,6 +42,7 @@ android {
 
 dependencies {
 
+    implementation("androidx.compose.material:material-icons-extended:<version>")
     implementation("androidx.room:room-runtime:2.6.1")
     kapt("androidx.room:room-compiler:2.6.1")
     implementation("androidx.room:room-ktx:2.6.1")
